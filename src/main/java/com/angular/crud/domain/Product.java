70e69f4240659744c1255e7e5d606bc43bf2521c
Sumit Bhoyar <sumitbhoyar@gmail.com>
2018-01-26 08:55:59 +0530

    Simple Entity with crud

---
+++ src/main/java/com/angular/crud/domain/Product.java
@@ -0,0 +1,115 @@
+package com.angular.crud.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A Product.
+ */
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @NotNull
+    @DecimalMin(value = "0")
+    @DecimalMax(value = "100000")
+    @Column(name = "price", precision=10, scale=2, nullable = false)
+    private BigDecimal price;
+
+    @NotNull
+    @Column(name = "jhi_date", nullable = false)
+    private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Product price(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Product date(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        if (product.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", price='" + getPrice() + "'" +
+            ", date='" + getDate() + "'" +
+            "}";
+    }
+}
